da638eb252d9eda5dbf2f2083e517908bcf49599
Nadiia Chernygina <nadja.chernygina@gmail.com>
2025-09-15 20:08:44 +0300

    test commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -45,3 +45,18 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     });
   });
 });
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  { threshold: 0.2 },
+);
+
+sections.forEach((section) => observer.observe(section));
